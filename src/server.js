ec76396176cfdb5f60abf6404b9e2a961f473dce
Ariful-Huq <amahuq@gmail.com>
2026-02-17 00:33:59 +0600

    twelveth commit

--- src/server.js
+++ src/server.js
@@ -10,13 +10,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello World changes'});
 });
 
 if (require.main === module) {
   // If the file is run directly, start the server
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
   server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
